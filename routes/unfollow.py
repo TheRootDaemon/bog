a2ea0623bb1975f81641b77e0ea2f6a279c427aa
TheRootDaemon <harshath3018@gmail.com>
2025-06-07 13:59:39 +0530

    Added a route to unfollow users

--- routes/unfollow.py
+++ routes/unfollow.py
@@ -11,35 +11,36 @@ router = APIRouter(
 )
 
 
-@router.post("/follow/{user_id}")
+@router.post("/unfollow/{user_id}")
 def follow(
     user_id: int,
     current_user: User = Depends(get_current_user),
     db: Session = Depends(get_db),
 ):
-    user_to_follow = db.query(User).filter(User.id == user_id).first()
+    user_to_unfollow = db.query(User).filter(User.id == user_id).first()
 
-    if not user_to_follow:
+    if not user_to_unfollow:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
         )
 
-    if current_user == user_to_follow:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, detail="You cannot follow yourself"
-        )
-
-    if user_to_follow in current_user.following:
+    if current_user == user_to_unfollow:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail="You are already following the user",
+            detail="You cannot unfollow yourself",
         )
 
-    current_user.following.append(user_to_follow)
+    if user_to_unfollow not in current_user.following:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="You are not following the user",
+        )
+
+    current_user.following.remove(user_to_unfollow)
     db.commit()
 
     return {
-        "message": f"You are now following {user_to_follow.username}",
+        "message": f"You are now not following {user_to_unfollow.username}",
         "following_count": len(current_user.following),
         "followers_count": len(current_user.followers),
     }
